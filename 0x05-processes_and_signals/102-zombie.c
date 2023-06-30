8691e6aba4a76253bc46178ef60dfffd641d54f3
Agun Akindele <aguntoroti10@gmail.com>
2023-06-30 12:57:26 -0700

    main

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * infinite_while - create a sleep loop
+ * Return: 0
+ */
+int infinite_white(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - Creates five zombie processes.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t pid;
+	char i = 1;
+
+	while (i <= 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			i++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
