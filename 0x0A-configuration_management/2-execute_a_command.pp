6414d8af8f8bbb1798c20c32e825b5c1f42cade9
root <aguntoroti10@gmail.com>
2023-08-25 05:12:48 -0700

    starting my process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# File: 2-execute_a_command.pp
+# Description: Kill a process named killmenow using exec resource
+
+exec { 'killmenow':
+command => 'pkill -f killmenow',
+onlyif  => 'pgrep -f killmenow',
+}
