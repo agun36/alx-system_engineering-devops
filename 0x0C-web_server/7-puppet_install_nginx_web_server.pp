20d64c61f8bf9696e4321eba00559d4080b604b8
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-29 09:38:03 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,31 +1,20 @@
-# install_nginx.pp
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
+}
 
-package {'nginx':
+file_line { 'aaaaa':
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec {'install':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx', # Use && instead of ;
-  path     => '/usr/bin', # Specify the path for the command
-  provider => shell,
-  require  => Package['nginx'], # Make sure the package is installed first
-}
-
-file {'/var/www/html/index.html':
-  ensure  => 'present',
+file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Exec['install'], # Make sure the file is created after installation
 }
 
-file_line {'redirect_rule':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'location /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }',
-  require => Exec['install'], # Make sure the file is modified after installation
-  notify  => Service['nginx'], # Notify Nginx to reload after modifying the file
-}
-
-service {'nginx':
+service { 'nginx':
   ensure  => running,
-  require => Exec['install'], # Make sure the service is started after installation
+  require => Package['nginx'],
 }
